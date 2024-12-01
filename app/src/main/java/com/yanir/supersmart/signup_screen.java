b92e4c32d6d63b222ccda701a9ebedf568418101
Yanir Aton <yanirahahah2007@gmail.com>
2024-12-01 12:08:11 +0200

    Initial commit: Added XML layout files for the app screens, including the basic structure and design. No functionality implemented yet.

---
+++ app/src/main/java/com/yanir/supersmart/signup_screen.java
@@ -0,0 +1,18 @@
+package com.yanir.supersmart;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class signup_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+    }
+}
